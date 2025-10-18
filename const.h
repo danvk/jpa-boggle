8530d09cb2739f5418e5b5c395825d2376fd234e
Dan Vanderkam <danvdk@gmail.com>
2025-10-18 12:45:06 -0400

    one thread

--- const.h
+++ const.h
@@ -26,8 +26,8 @@ unsigned long int CHILD_LETTER_BIT_MASKS[SIZE_OF_CHARACTER_SET];
 unsigned int CHILD_LETTER_BIT_SHIFTS[SIZE_OF_CHARACTER_SET];
 
 // Constants that define the high level "DeepSearch.c" algorithm.
-#define NUMBER_OF_WORKER_THREADS	4
-// #define NUMBER_OF_WORKER_THREADS	1
+// #define NUMBER_OF_WORKER_THREADS	4
+#define NUMBER_OF_WORKER_THREADS	1
 
 // Constants that are lexicon specific.
 #define TOTAL_WORDS_IN_LEXICON 44220
