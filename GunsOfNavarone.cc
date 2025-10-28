6330b4b27685b55caa81c3bc830746caa6f7d8c0
Dan Vanderkam <danvdk@gmail.com>
2025-10-28 17:05:48 -0400

    uint32_t, uint64_t

--- GunsOfNavarone.cc
+++ GunsOfNavarone.cc
@@ -36,19 +36,19 @@ using namespace std;
 // Constants that define the high level algorithm.
 #define NUMBER_OF_WORKER_THREADS 1
 
-unsigned int THE_SCORE_CARD[MAX_STRING_LENGTH + 1] = {
+uint32_t THE_SCORE_CARD[MAX_STRING_LENGTH + 1] = {
     0, 0, 0, 1, 1, 2, 3, 5, 11, 11, 11, 11, 11, 11, 11, 11
 };
 
 // These constant array's define the lexicon contained in the ADTDAWG.
-unsigned int CHARACTER_SET[SIZE_OF_CHARACTER_SET + 1] = {
+uint32_t CHARACTER_SET[SIZE_OF_CHARACTER_SET + 1] = {
     'A', 'C', 'D', 'E', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', ' '
 };
-unsigned int CHARACTER_LOCATIONS[NUMBER_OF_ENGLISH_LETTERS] = {
+uint32_t CHARACTER_LOCATIONS[NUMBER_OF_ENGLISH_LETTERS] = {
     0, BOGUS, 1,  2,     3,  BOGUS, 4,  BOGUS, 5,     BOGUS, BOGUS, 6,     7,
     8, 9,     10, BOGUS, 11, 12,    13, BOGUS, BOGUS, BOGUS, BOGUS, BOGUS, BOGUS
 };
-unsigned long int CHILD_LETTER_BIT_MASKS[SIZE_OF_CHARACTER_SET] = {
+uint64_t CHILD_LETTER_BIT_MASKS[SIZE_OF_CHARACTER_SET] = {
     1,
     6,
     24,
@@ -64,7 +64,7 @@ unsigned long int CHILD_LETTER_BIT_MASKS[SIZE_OF_CHARACTER_SET] = {
     2061584302080,
     32985348833280
 };
-unsigned int CHILD_LETTER_BIT_SHIFTS[SIZE_OF_CHARACTER_SET] = {
+uint32_t CHILD_LETTER_BIT_SHIFTS[SIZE_OF_CHARACTER_SET] = {
     0, 1, 3, 5, 8, 11, 14, 17, 21, 25, 29, 33, 37, 41
 };
 
@@ -78,8 +78,8 @@ struct Square {
   // The Used flag will indicate if a square is being used in constructing the current
   // word, and hence to remove the used square from further inclusion in the same word.
   bool used;
-  unsigned int letter_idx;
-  unsigned int num_neighbors;
+  uint32_t letter_idx;
+  uint32_t num_neighbors;
   Square *neighbors[NEIGHBOURS];
 };
 
@@ -87,7 +87,7 @@ struct Square {
 // board. Important note:  The function is going to use the low level C concept of
 // pointer arithmatic to fill the LivingNeighbourSquarePointerArray, which will be
 // filled from the top-left, clockwise.
-void SquareInit(Square *square, unsigned int row, unsigned int col) {
+void SquareInit(Square *square, uint32_t row, uint32_t col) {
   square->letter_idx = SIZE_OF_CHARACTER_SET;
   square->used = false;
   for (int i = 0; i < NEIGHBOURS; i++) {
@@ -188,8 +188,8 @@ struct Board {
 // ThisBoard's Block (this only needs to be done once). The Letter index for each square
 // will be a blank space ' ', and they will all be not Used.
 void BoardInit(Board *ThisBoard) {
-  unsigned int Row;
-  unsigned int Col;
+  uint32_t Row;
+  uint32_t Col;
   for (Row = MAX_ROW; Row-- > 0;) {
     for (Col = MAX_COL; Col-- > 0;) {
       SquareInit(&(ThisBoard->Block[Row][Col]), Row, Col);
@@ -202,8 +202,8 @@ void BoardInit(Board *ThisBoard) {
 // ADTDAWG's direct property enforces the "Order Does Not Matter," paradigm, and thus,
 // no sorting is required.
 void BoardPopulate(Board *bd, const char *letters) {
-  for (unsigned int Row = MAX_ROW; Row-- > 0;) {
-    for (unsigned int Col = MAX_COL; Col-- > 0;) {
+  for (uint32_t Row = MAX_ROW; Row-- > 0;) {
+    for (uint32_t Col = MAX_COL; Col-- > 0;) {
       (bd->Block)[Row][Col].letter_idx =
           CHARACTER_LOCATIONS[letters[Row * MAX_COL + Col] - 'A'];
     }
@@ -213,28 +213,28 @@ void BoardPopulate(Board *bd, const char *letters) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // These are the global variables needed.
 
-// Each worker thread will have it's own time stamping unsigned int array for all of the
+// Each worker thread will have it's own time stamping uint32_t array for all of the
 // words in the lexicon.
-unsigned int *LexiconMarks;
+uint32_t *LexiconMarks;
 
 // These are the pointers to the global immutable lexicon data structure.  The ADTDAWG
 // is well advanced and beyond the scope of the high level search algorithm. Since these
 // variables are branded as "Read Only," they can be utilized globally without passing
 // pointers.
-unsigned int *PartOneArray;
-unsigned long int *PartTwoArray;
-unsigned int *PartThreeArray;
+uint32_t *PartOneArray;
+uint64_t *PartTwoArray;
+uint32_t *PartThreeArray;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct DiscoveryStackNode {
   Square *TheSquareNow;
-  unsigned int LexiconPartOneIndex;
-  unsigned int FirstChildIndex;
-  unsigned int WordLengthNow;
-  unsigned int NextMarkerNow;
-  unsigned int TheChildToWorkOn;
-  unsigned long int TheSecondPartNow;
+  uint32_t LexiconPartOneIndex;
+  uint32_t FirstChildIndex;
+  uint32_t WordLengthNow;
+  uint32_t NextMarkerNow;
+  uint32_t TheChildToWorkOn;
+  uint64_t TheSecondPartNow;
 };
 
 // This is where the recursion-replacement stack is implemented.
@@ -275,16 +275,16 @@ DiscoveryStackNode TheDiscoveryStack[DISCOVERY_STACK_SIZE];
 
 int ScoreSquare(
     Square *square,
-    unsigned int part1_idx,
-    unsigned int lexicon_idx,
-    unsigned int mark,
-    unsigned int num_chars
+    uint32_t part1_idx,
+    uint32_t lexicon_idx,
+    uint32_t mark,
+    uint32_t num_chars
 ) {
-  unsigned int score = 0;
+  uint32_t score = 0;
   square->used = true;
 
   // Get the child index from the lexicon
-  unsigned int child_idx = (PartOneArray[part1_idx] & CHILD_MASK);
+  uint32_t child_idx = (PartOneArray[part1_idx] & CHILD_MASK);
 
   // Check if we have arrived at a new word, and if so, add the correct score
   if (PartOneArray[part1_idx] & END_OF_WORD_FLAG) {
@@ -298,21 +298,21 @@ int ScoreSquare(
   // If this node has children in the lexicon, explore the neighbors
   if (child_idx) {
     lexicon_idx -= PartThreeArray[child_idx];
-    unsigned long int part_two =
+    uint64_t part_two =
         PartTwoArray[(PartOneArray[part1_idx] & OFFSET_INDEX_MASK) >> OffSET_BIT_SHIFT];
 
     Square **neighbors = square->neighbors;
 
     // Loop through all neighbors
-    for (unsigned int X = 0; X < square->num_neighbors; X++) {
+    for (uint32_t X = 0; X < square->num_neighbors; X++) {
       auto n = neighbors[X];
       if (n->used == false) {
-        unsigned long int offset64 = (part_two & CHILD_LETTER_BIT_MASKS[n->letter_idx]);
+        uint64_t offset64 = (part_two & CHILD_LETTER_BIT_MASKS[n->letter_idx]);
 
         if (offset64) {
           offset64 >>= CHILD_LETTER_BIT_SHIFTS[n->letter_idx];
           offset64 -= 1;
-          auto offset32 = (unsigned int)offset64;
+          auto offset32 = (uint32_t)offset64;
 
           score += ScoreSquare(
               n,
@@ -331,13 +331,13 @@ int ScoreSquare(
 }
 
 // The function returns the Boggle score for "ThisBoard."
-unsigned int ScoreBoard(Board *ThisBoard, unsigned int mark) {
+uint32_t ScoreBoard(Board *ThisBoard, uint32_t mark) {
   auto &block = ThisBoard->Block;
-  unsigned int score = 0;
+  uint32_t score = 0;
   // Add up all the scores that originate from each square in the board.
-  for (unsigned int row = 0; row < MAX_ROW; row++) {
-    for (unsigned int col = 0; col < MAX_COL; col++) {
-      unsigned int part1_idx = block[row][col].letter_idx + 1;
+  for (uint32_t row = 0; row < MAX_ROW; row++) {
+    for (uint32_t col = 0; col < MAX_COL; col++) {
+      uint32_t part1_idx = block[row][col].letter_idx + 1;
       score +=
           ScoreSquare(&block[row][col], part1_idx, PartThreeArray[part1_idx], mark, 1);
     }
@@ -354,9 +354,9 @@ int LoadDictionary() {
   FILE *PartFour = fopen(FOUR_PART_DTDAWG_14_PART_FOUR, "rb");
 
   unsigned char TempReadIn;
-  unsigned int SizeOfPartOne;
-  unsigned long int SizeOfPartTwo;
-  unsigned int SizeOfPartThree;
+  uint32_t SizeOfPartOne;
+  uint64_t SizeOfPartTwo;
+  uint32_t SizeOfPartThree;
 
   // Read in the size of each data file.
   if (fread(&SizeOfPartOne, 4, 1, PartOne) != 1) return 0;
@@ -364,12 +364,12 @@ int LoadDictionary() {
   if (fread(&SizeOfPartThree, 4, 1, PartThree) != 1) return 0;
 
   // Allocate memory for the ADTDAWG.
-  size_t bytes_one = (SizeOfPartOne + 1) * sizeof(int);
-  size_t bytes_two = SizeOfPartTwo * sizeof(long int);
-  size_t bytes_three = (SizeOfPartOne + 1) * sizeof(int);
-  PartOneArray = (unsigned int *)malloc(bytes_one);
-  PartTwoArray = (unsigned long int *)malloc(bytes_two);
-  PartThreeArray = (unsigned int *)malloc(bytes_three);
+  size_t bytes_one = (SizeOfPartOne + 1) * sizeof(uint32_t);
+  size_t bytes_two = SizeOfPartTwo * sizeof(uint64_t);
+  size_t bytes_three = (SizeOfPartOne + 1) * sizeof(uint32_t);
+  PartOneArray = (uint32_t *)malloc(bytes_one);
+  PartTwoArray = (uint64_t *)malloc(bytes_two);
+  PartThreeArray = (uint32_t *)malloc(bytes_three);
 
   printf(
       "bytes for arrays: %zu + %zu + %zu = %zu\n",
@@ -403,9 +403,9 @@ int LoadDictionary() {
 }
 
 int main(int argc, char *argv[]) {
-  unsigned int X;
-  unsigned int CurrentScore;
-  unsigned int BoardCount = 0;
+  uint32_t X;
+  uint32_t CurrentScore;
+  uint32_t BoardCount = 0;
 
   char BoardString[SQUARE_COUNT + 1];
   Board *WorkingBoard = (Board *)malloc(sizeof(Board));
@@ -424,13 +424,13 @@ int main(int argc, char *argv[]) {
 
   BoardInit(WorkingBoard);
 
-  // Allocate the set of lexicon time stamps as unsigned integers.
-  size_t bytes_marks = (TOTAL_WORDS_IN_LEXICON + 1) * sizeof(unsigned int);
-  LexiconMarks = (unsigned int *)malloc(bytes_marks);
+  // Allocate the set of lexicon time stamps as uint32_tegers.
+  size_t bytes_marks = (TOTAL_WORDS_IN_LEXICON + 1) * sizeof(uint32_t);
+  LexiconMarks = (uint32_t *)malloc(bytes_marks);
   printf("bytes for marks: %zu\n", bytes_marks);
 
   // Zero all of the global time stamps.
-  memset(LexiconMarks, 0, (TOTAL_WORDS_IN_LEXICON + 1) * sizeof(unsigned int));
+  memset(LexiconMarks, 0, (TOTAL_WORDS_IN_LEXICON + 1) * sizeof(uint32_t));
 
   int result = LoadDictionary();
   if (result != 1) {
@@ -459,7 +459,7 @@ int main(int argc, char *argv[]) {
   }
   fclose(input_file);
 
-  unsigned int total_score = 0;
+  uint32_t total_score = 0;
   for (const auto &board : boards) {
     BoardPopulate(WorkingBoard, board.c_str());
     CurrentScore = ScoreBoard(WorkingBoard, BoardCount + 1);
